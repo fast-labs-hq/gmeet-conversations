bf0dd73e548b61f1ea45e5184ce6642ca3193c9e
Sagar <sagar.sehgal@opendoor.com>
2024-05-05 23:43:35 +0530

    added ability to save in local storage and read chat from local storage

--- index.js
+++ index.js
@@ -1,67 +1,67 @@
-// alert("taking note of all the conversations :D")
-
-// function send_mail(to_email, meeting_chat){
-
-//     var data = {
-//         service_id: 'service_6ar5ioo',
-//         template_id: 'template_h3tvqdj',
-//         user_id: '2I8ka5tfwSy8zKte0',
-//         template_params: {
-//             to_name: "User",
-//             meeting_title: "Meeting Title",
-//             meeting_chat: meeting_chat,
-//             to_email: to_email,
-//         }
-//     };
-
-//     $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
-//         type: 'POST',
-//         data: JSON.stringify(data),
-//         contentType: 'application/json'
-//     }).done(function() {
-//         console.log('Your mail is sent!');
-//     }).fail(function(error) {
-//         console.log('Error... ' + JSON.stringify(error));
-//     });
-// }
-
-
-
-
-// var chat = document.getElementsByClassName("z38b6")
-// var config = { attributes: true, childList: true, subtree: true };
-
-// var observer = new MutationObserver((list) => {
-//     console.log("yes working on it");
-//     console.log(list[0].target.textContent);
-// });
-
-// document.getElementsByClassName("VfPpkd-Bz112c-LgbsSe yHy1rc eT1oJ JsuyRc boDUxc")[2].click()
-
 var observer = new MutationObserver(() => {
-    var all_messages = document.getElementsByClassName("Ss4fHf")
-    const meeting_id = document.getElementsByClassName("u6vdEc")[0].innerText
+    var all_messages = document.getElementsByClassName("Ss4fHf");
     chat = []
     for(let i=0;i<all_messages.length;i++) {
         message_list = []
         for (let j=0;j < all_messages[i].getElementsByClassName("beTDc").length; j++) {
-            message_list.push(all_messages[i].getElementsByClassName("beTDc")[j].innerText)
+            let contentDivs = all_messages[i].getElementsByClassName("beTDc")[j].querySelectorAll('div[jsaction="rcuQ6b:XZyPzc"]');
+            for (let div of contentDivs) {
+                message_list.push(div.innerText.trim());
+            }
         }
         message = {
             "from": all_messages[i].getElementsByClassName("poVWob")[0].innerText,
-            "time": all_messages[i].getElementsByClassName("MuzmKe")[0].innerText,
+            "time": all_messages[i].getElementsByClassName("MuzmKe").length > 0 ? all_messages[i].getElementsByClassName("MuzmKe")[0].innerText : "",
             "message": message_list
         }
         chat.push(message)
     };
-    console.log(JSON.stringify(chat))
-    chrome.storage.local.set({ 'key': 'value' }, function() {
-        console.log('Meeting data has been stored');
-    }).then(
-        chrome.storage.local.get(['key'], function(result){
-            console.log(result)
+
+    // to keep records of all gmeet conversation ids in sequence 
+    chrome.storage.local.get(["all_gmeets"], function(result){
+        const meeting_id = document.getElementsByClassName("u6vdEc")[0].innerText;
+        
+        // Initialize an empty object if gmeet_chat_v1 key doesn't exist
+        if (!result.hasOwnProperty("all_gmeets")) {
+            result["all_gmeets"] = [];
         }
-    ));          
+
+        if(!result["all_gmeets"].includes(meeting_id)) {
+            result["all_gmeets"].push(meeting_id)
+        }
+
+        // Update the storage with the modified result object
+        chrome.storage.local.set({"all_gmeets": result["all_gmeets"]}, function() {
+            console.log('Data has been appended to the `all_gmeets` array in local storage.');
+            console.log(result["all_gmeets"])
+        });
+
+    });
+    
+    // to keep records of all gmeet conversations as a dictionary  
+    chrome.storage.local.get(["gmeet_chat_v1"], function(result) {
+        const meeting_id = document.getElementsByClassName("u6vdEc")[0].innerText;
+        
+        // Initialize an empty object if gmeet_chat_v1 key doesn't exist
+        if (!result.hasOwnProperty("gmeet_chat_v1")) {
+            result["gmeet_chat_v1"] = {};
+        }
+        
+        // Initialize an empty array if meeting_id key doesn't exist
+        if (!result["gmeet_chat_v1"].hasOwnProperty(meeting_id)) {
+            result["gmeet_chat_v1"][meeting_id] = [];
+        }
+        
+        // Push chat object to the array
+        result["gmeet_chat_v1"][meeting_id] = chat;
+    
+        console.log(result);
+        
+        // Update the storage with the modified result object
+        chrome.storage.local.set({"gmeet_chat_v1": result["gmeet_chat_v1"]}, function() {
+            console.log('Data has been appended to the array in local storage.');
+        }); 
+    });
 });
 
 
@@ -84,5 +84,6 @@ window.onload = async () => {
         chat = document.getElementsByClassName("z38b6")
     }
     var config = { attributes: true, childList: true, subtree: true };
+
     observer.observe(chat[0], config)
 }
--- popup.js
+++ popup.js
@@ -1,1 +1,16 @@
-console.log("hello world")
+document.getElementById("myButton").addEventListener("click", clickMe);
+
+
+function clickMe() {
+  chrome.storage.local.get('gmeet_chat_v1', function(result) {
+    console.log("loaded data",result);
+
+    var chatDataElement = document.getElementById("chat_data");
+    if (result['gmeet_chat_v1']) {
+      chatDataElement.innerText = JSON.stringify(result['gmeet_chat_v1']);
+    } else {
+      chatDataElement.innerText = 'No data found';
+    }
+
+  });
+}
