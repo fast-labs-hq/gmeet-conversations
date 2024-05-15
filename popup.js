27483eee81ceaf110b7dc8911f8297d8f9cf749e
thesagarsehgal <sagarsehgal.tech@gmail.com>
2024-05-15 13:32:36 +0530

    adding ability to record time of the meeting and showing in sorted order

--- popup.js
+++ popup.js
@@ -6,15 +6,32 @@ window.onload = function () {
     let chatDataElement = document.getElementById("chat_data");
     if (result["gmeet_chat_v1"]) {
       const gMeets = Object.keys(result["gmeet_chat_v1"]);
+
+      // Sort the gMeets array based on first_reported_time
+      gMeets.sort((a, b) => {
+        let timeA = result["gmeet_chat_v1"][a]["first_reported_time"];
+        let timeB = result["gmeet_chat_v1"][b]["first_reported_time"];
+
+        if (timeA && timeB) {
+          return new Date(timeA) - new Date(timeB);
+        } else if (timeA) {
+          return -1;
+        } else if (timeB) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+
       gMeets.forEach((meet) => {
         let divElement = document.createElement("div");
         divElement.classList.add("chat-container");
 
         divElement.innerHTML = `
-        <h2>${meet}</h2>
+        <h2>${meet}</h2> <h3>(${result["gmeet_chat_v1"][meet]["first_reported_time"]})</h3>
         `;
 
-        result["gmeet_chat_v1"][meet].forEach((info) => {
+        result["gmeet_chat_v1"][meet]["chat"].forEach((info) => {
           let innerDivElement = document.createElement("div");
 
           innerDivElement.innerHTML = `
